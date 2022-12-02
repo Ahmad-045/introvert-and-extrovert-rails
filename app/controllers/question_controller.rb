47a7c88e7db4ea80e24391ead9c956e6ccd4a740
Ahmad-045 <ahmad.amin@devsinc.com>
2022-12-02 15:34:02 +0500

    Changed from DataBase to Local Constant

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -4,7 +4,7 @@ class QuestionController < ApplicationController
   @@results = {}
 
   def index
-    @question = Question.first
+    @question = Question::DATA[0]
     if @@results.length == 5
       @ur_personality = @@results.values.max_by {|i| @@results.values.count(i)}
     end
@@ -13,14 +13,17 @@ class QuestionController < ApplicationController
 
   def show
     @question_count = @@results.length + 1
-    @question = Question.where.not(id: @@results.keys).order("RANDOM()").first
+    q_index = ([*1..(Question::DATA.length)] - @@results.keys).sample
+    @question = Question::DATA.select{|obj| obj[:id] == q_index}.sample
 
     redirect_to root_path if @@results.length == 5
   end
 
   def add_result
-    @@results[params[:question_id]] = params[:option_type]
+    @@results[params[:question_id].to_i] = params[:option_type]
     redirect_to question_path(params[:question_id])
   end
+
+  private
 
 end
