37289ba4fe63526434f79b8c8488d8789b0ca94c
Ahmad-045 <ahmad.amin@devsinc.com>
2022-12-09 13:59:39 +0500

    Added the CREATE Function

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -17,13 +17,37 @@ class QuestionController < ApplicationController
     # because select returns an array with matching fields.,
     # sample is used to take randomly any one from the array as an object
 
-    @question = JSON.parse(REDIS.get('QUESTIONS')).select{|obj| !@@results.key?(obj[:id])}.sample
+    @question = JSON.parse(REDIS.get('QUESTIONS')).select{|obj| !@@results.key?(obj['id'])}.sample
     redirect_to root_path if @@results.length == 5
   end
+
+  def create
+    reform_data_to_add
+  end
+
 
   def add_result
     @@results[params[:question_id].to_i] = params[:option_type]
     redirect_to question_path(params[:question_id])
+  end
+
+
+  private
+  def reform_data_to_add
+    byebug
+    questionData = JSON.parse(REDIS.get('QUESTIONS'))
+    data = {
+      id: questionData[questionData.length-1]['id'] + 1,
+      content: params[:question_content],
+      options: [
+        { content: params[:option_1], option_type: params[:option_type_1] },
+        { content: params[:option_2], option_type: params[:option_type_2] },
+        { content: params[:option_3], option_type: params[:option_type_3] },
+        { content: params[:option_4], option_type: params[:option_type_4] }
+      ]
+    }
+    questionData.push(data)
+    REDIS.set('QUESTIONS', questionData.to_json)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
 
   post '/add_result', to: 'question#add_result', as: 'add_result'
 
+  match '*unmatched', to: 'application#not_found_method', via: :all
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
